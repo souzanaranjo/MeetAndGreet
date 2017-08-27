be98fc9f8f60b39a7761eb7983753a5c669ec7e0
David Souza <davidsouza@Davids-MacBook-Pro.local>
2017-08-26 19:38:49 -0500

    create

---
+++ app/assets/javascripts/register.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    alert("Hola");
+    $("#ciudadNombre").oninput = autoCompletar();
+    $('#intereses').tagsinput({
+                maxTags:4,
+                typeahead: {
+                    source: ['Buea', 'Douala', 'Kribi', 'Yaounde']
+                }
+    });
+});
+
+
+function autoCompletar() {
+    var options = {
+                      types: ['(cities)'],
+                      componentRestrictions: {country: "mx"}
+                      };
+    var input = document.getElementById('ciudadNombre');
+    var autocomplete = new google.maps.places.Autocomplete(input, options);
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  resources :home
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    devise_scope :user do
+        
+        
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root 'home#index', as: :unauthenticated_root
+    end
+  end
 end
